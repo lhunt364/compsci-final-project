9b81fc7c0a892f43d0d72b646df48f2c66719e78
lhunt364 <105075317+lhunt364@users.noreply.github.com>
2022-05-11 13:52:12 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,57 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-public class Main extends JFrame implements ActionListener
+public class Main extends JFrame implements ActionListener, MouseListener
 {
 	private Timer t;
+	private char state; //the current state of the game (m = menu, p = playing)
 	
 	public Main()
 	{
+		state = 'm';
 		//set up frame stuff
 		setBounds(100,100,600,600);
 		setTitle("joe");
 		setResizable(false);
-		setBackground(Color.lightGray);
 		
-		
+		//set up panel
 		MainPanel p = new MainPanel();
 		add(p);
+		p.setPreferredSize(new Dimension(600,600));
+		
+		//add key listener
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) 
+			{
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) 
+			{
+				if(e.getKeyCode() == 'w') ;
+				if(e.getKeyCode() == 's') ;
+				if(e.getKeyCode() == 'a') ;
+				if(e.getKeyCode() == 'd') ;
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) 
+			{
+
+			}
+			
+		});
 		
 		
 		//finish frame stuff
@@ -49,6 +81,24 @@ public class Main extends JFrame implements ActionListener
 @SuppressWarnings("serial")
 class MainPanel extends JPanel
 {
+	//set up panel
+	public MainPanel()
+	{
+		setBackground(Color.lightGray);
+	}
+	
+	//draw stuff
+	Player player;
+	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	ArrayList<Wall> walls = new ArrayList<Wall>();
+	ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+	
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+	}
+	
+	
 	
 }
 
