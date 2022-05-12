4a50f6e2b3190d08846b10f479ceb59a9acff3e5
logan <loganhunt364@gmail.com>
2022-05-12 14:16:49 -0400

    joe

--- Wall.java
+++ Wall.java
@@ -1,5 +1,38 @@
 
-public class Wall {
-	private int length;
+public class Wall
+{
+	private int x;
+	private int y;
+	private int height;
+	private int width;
+
+	public Wall(int x, int y, int length, int height)
+	{
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public int getWidth()
+	{
+		return width;
+	}
+
+	public int getHeight()
+	{
+		return height;
+	}
 	//test
+
 }
