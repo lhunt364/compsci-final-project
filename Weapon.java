7548062b2a6b3b44ce055431bf00235f024e4550
logan <loganhunt364@gmail.com>
2022-05-12 14:01:12 -0400

    joe

---
+++ Weapon.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Weapon
+{
+    private String name;
+    private int dmg;
+    private int fireRate;
+    private String fireSound;
+    private String reloadSound;
+    private Color color;
+
+    public Weapon(String name, int dmg, int fireRate, String fireSound, String reloadSound, Color color)
+    {
+        this.name = name;
+        this.dmg = dmg;
+        this.fireRate = fireRate;
+        this.fireSound = fireSound;
+        this.reloadSound = reloadSound;
+        this.color = color;
+
+    }
+}
